6e09d35081b9db811d474c54c7e24330befb7b1e
unknown <ekubler@student.agh.edu.pl>
2026-05-21 19:34:54 +0200

    Poprawiona wersja zadania - Eryk Kubler

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/laboratorium_nr5_ErykKubler/',
+  plugins: [
+    tailwindcss(),
+  ],
 })
